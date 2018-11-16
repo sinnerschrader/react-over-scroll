8374a43097fa9c2fb0955edc95543ad4bce3cc01
Gregor Adams <greg@pixelass.com>
2018-11-16 02:46:49 +0100

    chore: content based logic
    BREAKING CHANGE: API changes

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export {ScrollConsumer as Listener} from "./context";
+export {default} from "./overscroll";
+export {progressable} from "./progressable";
--- src/overscroll.tsx
+++ src/overscroll.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import {ScrollConsumer, ScrollProvider} from "./context";
 import Tracker from "./tracker";
 
-type TRenderer = (page: number, progress: number) => any;
+export type TRenderer = (IContext) => any;
+export type TChild = (IContext) => any;
 
-interface IOverScrollProps {
+export interface IOverScrollProps {
 	anchors?: string;
-	children?: TRenderer;
+	children?: TChild;
 	className?: string;
 	factor?: number;
 	render?: TRenderer;
@@ -13,29 +15,30 @@ interface IOverScrollProps {
 	throttle?: number;
 }
 
-interface IOverScrollState {
+export interface IOverScrollState {
 	counter: number;
 	scrollOffset: number;
 	scrollY: number;
 }
 
+const fitTop: React.CSSProperties = {
+	left: 0,
+	right: 0,
+	top: 0
+};
 /**
  * @return {Object} returns a position sticky polyfill
  */
 const overlayStyle: React.CSSProperties = {
+	...fitTop,
 	height: "100vh",
-	left: 0,
-	position: "sticky",
-	right: 0,
-	top: 0
+	position: "sticky"
 };
 
 const anchorStyle: React.CSSProperties = {
+	...fitTop,
 	bottom: 0,
-	left: 0,
-	position: "absolute",
-	right: 0,
-	top: 0
+	position: "absolute"
 };
 
 class OverScroll extends React.Component<IOverScrollProps, IOverScrollState> {
@@ -58,44 +61,78 @@ class OverScroll extends React.Component<IOverScrollProps, IOverScrollState> {
 	}
 
 	public render() {
-		const {render, children, throttle: throttleValue} = this.props;
-		const renderer = render || children;
-		if (!renderer) {
-			throw new TypeError(`Either render or children needs to be defined as a renderer`);
+		const {render, children, anchors, slides: pages} = this.props;
+		const pass = render || children;
+		if (!pass) {
+			throw new TypeError(
+				`Either children need to be defined or render needs to be defined as a function`
+			);
 		}
-		return (
-			<React.Fragment>
-				<Tracker onScroll={this.handleScroll} throttle={throttleValue} />
-				<div style={this.frameStyle} ref={this.tracker}>
-					{this.anchors}
-					<div style={overlayStyle}>{renderer(this.state.counter, this.state.scrollOffset / 100)}</div>
-				</div>
-			</React.Fragment>
-		);
+		const {counter: page} = this.state;
+		const progress = this.state.scrollOffset / 100;
+		switch (typeof render) {
+			case "function":
+				return (
+					<this.Frame>
+						{render({
+							anchors,
+							page,
+							pages,
+							progress
+						})}
+					</this.Frame>
+				);
+			default:
+				return (
+					<ScrollProvider value={{anchors, page, pages, progress}}>
+						<this.Frame>
+							<ScrollConsumer>{children}</ScrollConsumer>
+						</this.Frame>
+					</ScrollProvider>
+				);
+		}
 	}
 
 	/**
+	 * THis function provides a frame to allow OverScroll to work.
+	 * @param {{children: any}} props
+	 * @constructor
+	 */
+	private Frame = (props: {children: any}) => (
+		<React.Fragment>
+			<Tracker onScroll={this.handleScroll} throttle={this.props.throttle} />
+			<div style={this.frameStyle} ref={this.tracker}>
+				{this.anchors}
+				<div style={overlayStyle}>{props.children}</div>
+			</div>
+		</React.Fragment>
+	);
+
+	/**
 	 * checks for the current position and translates the scroll to index and percent
-	 * @param  {number} scrollY - window.scrollY
+	 * @param {number} scrollY - window.scrollY
 	 */
 	private handleScroll = (scrollY: number): void => {
 		// make sure the tracker element exists
 		if (!this.tracker || !this.tracker.current) {
 			return;
 		}
-		let counter = 0;
-		let scrollOffset = 0;
+		let counter: number = 0;
+		let scrollOffset: number = 0;
 
 		// get the offset and check if the top or bottom have been reached
 		// top activates the snap mode
 		// bottom deactivates the snap mode
-		const {top, bottom} = this.tracker.current.getBoundingClientRect();
+		const {top, bottom}: ClientRect = this.tracker.current.getBoundingClientRect();
 		const {factor, slides} = this.props;
-		const {innerHeight = 0} = window;
-		const touchedTop = top <= 0;
-		const touchedEnd = bottom <= innerHeight;
+		const {innerHeight = 0}: Window = window;
+		const touchedTop: boolean = top <= 0;
+		const touchedEnd: boolean = bottom <= innerHeight;
 		if (touchedTop && !touchedEnd) {
-			counter = Math.max(0, Math.min(slides - 1, Math.floor((top * (-1 / factor)) / innerHeight)));
+			counter = Math.max(
+				0,
+				Math.min(slides - 1, Math.floor((top * (-1 / factor)) / innerHeight))
+			);
 			scrollOffset = Math.max(
 				0,
 				Math.min(100, (((top * -1) % (innerHeight * factor)) / innerHeight / factor) * 100)
